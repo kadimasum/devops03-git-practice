465e812ba4387ed67dac179e848af9a2296886c6
Mariam Senzia <mariamsenzia@gmail.com>
2023-06-18 08:44:53 +0300

    modified folder

---
+++ Mariam-Senzia/test.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+#################
+# Author : Mariam
+# Date : 18/06/2023
+#
+#This script creates files and folders 
+#################
+
+#create folder
+mkdir moringadevops
+
+#create file
+touch file1.txt
+
+echo "created file and folder"
